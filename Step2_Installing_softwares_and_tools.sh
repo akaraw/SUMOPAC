33769e091d153dc66ec3db1e6f81023e738b2c58
Anjana_CK <anjana.karawita@csiro.au>
2022-11-07 15:03:30 +1100

    Update Step2_Installing_softwares_and_tools.sh

--- Step2_Installing_softwares_and_tools.sh
+++ Step2_Installing_softwares_and_tools.sh
@@ -75,7 +75,7 @@ then
     git clone https://github.com/lh3/minimap2
     cd minimap2 && make
     echo "export PATH=$basedir/minimap2/minimap2:$PATH" >> ~/.bashrc #wiritning the pathway export to bashrc to mount it automatically
-    export PATH=$basedir/minimap2:$PATH
+    export PATH=$basedir/minimap2/minimap2:$PATH
 fi
 
 ###################
