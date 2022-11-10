cfe759d9c915cfa84368c13fa257d79afffe2f1a
Anjana_CK <anjana.karawita@csiro.au>
2022-11-10 15:54:58 +1100

    Update Step2_Installing_softwares_and_tools.sh

--- Step2_Installing_softwares_and_tools.sh
+++ Step2_Installing_softwares_and_tools.sh
@@ -2,9 +2,9 @@
 #**Step 2**
 
 #################################################################################################################
-#This step describes the method for installing necessary programs to run the analysis on MinIon seqeuencing data. 
-#Please cite each software appropriately                                                                          
-#installing the bioinformatics tools                                                                              
+#This step describes the method for installing necessary programs to run the analysis on MinIon seqeuencing data.
+#Please cite each software appropriately
+#installing the bioinformatics tools
 #################################################################################################################
 basedir=$1
 export PATH=$basedir/bin:$PATH
@@ -47,15 +47,15 @@ then
     echo "R could not be found. Please install R"
     exit 1
 else
-    echo "R found - proceeding to the next step"    
+    echo "R found - proceeding to the next step"
 fi
-  
+
 if ! command -v git &> /dev/null
 then
     echo "git could not be found. Please install git"
     exit 1
 else
-    echo "git found - proceediong to the next step"    
+    echo "git found - proceediong to the next step"
 fi
 
 if ! command -v make &> /dev/null
@@ -63,7 +63,7 @@ then
     echo "make could not be found. Please install"
     exit 1
 else
-    echo "make found - proceeding to the next step"    
+    echo "make found - proceeding to the next step"
 fi
 
 ###########################################
@@ -83,12 +83,12 @@ fi
 ###################
 if ! command -v kraken2 &> /dev/null
 then
-    git clone https://github.com/DerrickWood/kraken2.git 
+    git clone https://github.com/DerrickWood/kraken2.git
     cd kraken2
     KRAKEN2_DIR=$basedir/bin
     ./install_kraken2.sh $KRAKEN2_DIR
     echo "export PATH=$basedir/bin:$PATH" >> ~/.bashrc
-    export PATH=$basedir/bin:$PATH
+    export PATH=$basedir/bin:$PAT
 fi
 
 
@@ -104,7 +104,7 @@ fi
 
 #####################################################################################################################
 #Finally you need to mount all the executable paths into ./bashrc so that next time when start the Ubuntu terminal, \
-#it will automatically export the paths to the executables 
+#it will automatically export the paths to the executables
 #####################################################################################################################
 #Then restart the terminal
 #Now you can test your installation
@@ -115,7 +115,7 @@ then
     echo "minimap2 could not be found. Please install"
     exit 1
 else
-    echo "minimap2 found - checking kraken2"    
+    echo "minimap2 found - checking kraken2"
 fi
 
 if ! command -v kraken2 &> /dev/null
@@ -123,27 +123,27 @@ then
     echo "kraken2 could not be found. Please install"
     exit 1
 else
-    echo "kraken2 found - now installing NCBI Blast"    
+    echo "kraken2 found - now installing NCBI Blast"
 fi
 
 #######################################################################
 #Install NCBI Blast tools for Kraken2 repeat masking steps - dustmasker
 #######################################################################
 if ! command -v dustmasker > /dev/null; then
-	echo "dustmasker is not in the path. Please check the installation"
-	echo "We will now install it"
-	if [ -d $basedir/ncbi-blast-2.13.0+/bin ]; then
-		echo "export PATH=$basedir/ncbi-blast-2.13.0+/bin:$PATH" >> ~/.bashrc
-		export PATH=$basedir/ncbi-blast-2.13.0+/bin/:$PATH
-	else
-		wget https://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz -O $basedir/ncbi-blast-2.13.0+-x64-linux.tar.gz
-		tar zxvpf $basedir/ncbi-blast-2.*+-x64-linux.tar.gz --directory $basedir
-		echo "export PATH=$basedir/ncbi-blast-2.13.0+/bin:$PATH" >> ~/.bashrc
-		export PATH=$basedir/ncbi-blast-2.13.0+/bin/:$PATH
-	fi
+        echo "dustmasker is not in the path. Please check the installation"
+        echo "We will now install it"
+        if [ -d $basedir/ncbi-blast-2.13.0+/bin ]; then
+                echo "export PATH=$basedir/ncbi-blast-2.13.0+/bin:$PATH" >> ~/.bashrc
+                export PATH=$basedir/ncbi-blast-2.13.0+/bin/:$PATH
+        else
+                wget https://ftp.ncbi.nlm.nih.gov/blast/executables/LATEST/ncbi-blast-2.13.0+-x64-linux.tar.gz -O $basedir/ncbi-blast-2.13.0+-x64-linux.tar.gz
+                tar zxvpf $basedir/ncbi-blast-2.*+-x64-linux.tar.gz --directory $basedir
+                echo "export PATH=$basedir/ncbi-blast-2.13.0+/bin:$PATH" >> ~/.bashrc
+                export PATH=$basedir/ncbi-blast-2.13.0+/bin/:$PATH
+        fi
 else
-	echo "dustmasker is installed"
-	echo " Creating KRAKEN databses"
+        echo "dustmasker is installed"
+        echo " Creating KRAKEN databses"
 fi
 
 if ! command -v dustmasker > /dev/null; then
@@ -157,21 +157,21 @@ fi
 DBBAC=$basedir/kraken2bac
 if ! [ -d $DBBAC ]; then
 
-	#DBBAC=$basedir/kraken2bac
-	kraken2-build --threads 6 --download-taxonomy --db $DBBAC
-	kraken2-build --threads 6 --download-library bacteria --db $DBBAC
-	kraken2-build --threads 6 --threads 6 --build --db $DBBAC
-	#if above command give you an error, please read here https://github.com/DerrickWood/kraken2/issues/508
+        #DBBAC=$basedir/kraken2bac
+        kraken2-build --threads 6 --download-taxonomy --db $DBBAC
+        kraken2-build --threads 6 --download-library bacteria --db $DBBAC
+        kraken2-build --threads 6 --threads 6 --build --db $DBBAC
+        #if above command give you an error, please read here https://github.com/DerrickWood/kraken2/issues/508
 fi
 
 #Since the bacterial library is quite large, it is better to create anindex of the lib for the minimap2 run as follows
 LIB=$DBBAC/library/bacteria/library.fna
 if ! [ -f $LIB ]; then
-	#IB=$DBBAC/library/bacteria/library.fna
-	AXMAP=$basedir/bactaxmap.tab
-	grep ">"  $LIB | sed 's/>.*|//g' | cut -d" " -f1,2,3,4 | sed -r 's/\s+/\t/' > $TAXMAP
-	$INDEX=$basedir/bac.mmi
-	minimap2 -k 13 -t 12 -d $INDEX $LIB
+        #IB=$DBBAC/library/bacteria/library.fna
+        AXMAP=$basedir/bactaxmap.tab
+        grep ">"  $LIB | sed 's/>.*|//g' | cut -d" " -f1,2,3,4 | sed -r 's/\s+/\t/' > $TAXMAP
+        $INDEX=$basedir/bac.mmi
+        minimap2 -k 13 -t 12 -d $INDEX $LIB
 fi
 
 ##################################
@@ -180,29 +180,29 @@ fi
 
 DBVIR=$basedir/kraken2vir
 if ! [ -d $DBVRI ];then
-	kraken2-build --threads 6 --download-taxonomy --db $DBVIR
-	kraken2-build --threads 6 --download-library viral --db $DBVIR
-	kraken2-build --threads 6 --threads 6 --build --db $DBVIR
+        kraken2-build --threads 6 --download-taxonomy --db $DBVIR
+        kraken2-build --threads 6 --download-library viral --db $DBVIR
+        kraken2-build --threads 6 --threads 6 --build --db $DBVIR
 fi
 
 LIB=$DBVIR/library/viral/library.fna
 
 if ! [ -f $LIB ]; then
-	TAXMAP=$basedir/virtaxmap.tab
-	grep ">"  $LIB | sed 's/>.*|//g' | cut -d" " -f1,2,3,4 | sed -r 's/\s+/\t/' > $TAXMAP
+        TAXMAP=$basedir/virtaxmap.tab
+        grep ">"  $LIB | sed 's/>.*|//g' | cut -d" " -f1,2,3,4 | sed -r 's/\s+/\t/' > $TAXMAP
 fi
 
 #######################################################################################################################
 #Now lets dwonload some more genomes for vectors (mozies) as per the instrcution below:
-#First install the ncbi_datasets tools: 
+#First install the ncbi_datasets tools:
 #Follow the instruction here for linux installation https://www.ncbi.nlm.nih.gov/datasets/docs/v2/download-and-install/
 #Once it is installed:
 #######################################################################################################################
 DBVEC=$basedir/kraken2vec
 if ! [ -s $DBVEC/taxo.k2d ]; then
-	kraken2-build --threads 6 --download-taxonomy --db $DBVEC
+        kraken2-build --threads 6 --download-taxonomy --db $DBVEC
 else
-	echo "$DBVEV seems to be present. Skipping creating it"
+        echo "$DBVEV seems to be present. Skipping creating it"
 fi
 
 #Using NCBI datasets tools
@@ -218,23 +218,23 @@ fi
 
 #IF you see the path, it is working
 #now lets download some data
- 
+
 genus=anopheles
 
 if ! [ -s $genus/ncbi_dataset/data ];then
-	echo "Downloadng $genus.... We will let you know when this is done."
-	datasets download genome taxon anopheles --dehydrated --filename anopheles.zip #--assembly-level complete #Once this is completed
-	unzip anopheles.zip -d anopheles
-	datasets rehydrate --directory anopheles #This will run for a while (30 to 100 mnutes depending on the connection speed)
-	for i in $genus/ncbi_dataset/data/*/*.fna; do kraken2-build --add-to-library $i --db $DBVEC; done
-	echo "$genus added to the database"
+        echo "Downloadng $genus.... We will let you know when this is done."
+        datasets download genome taxon anopheles --dehydrated --filename anopheles.zip #--assembly-level complete #Once this is completed
+        unzip anopheles.zip -d anopheles
+        datasets rehydrate --directory anopheles #This will run for a while (30 to 100 mnutes depending on the connection speed)
+        for i in $genus/ncbi_dataset/data/*/*.fna; do kraken2-build --add-to-library $i --db $DBVEC; done
+        echo "$genus added to the database"
 else
-	echo "$genus already downloaded. We assume it is already addedd to the $DBVEC"
+        echo "$genus already downloaded. We assume it is already addedd to the $DBVEC"
 fi
 
 #For minimap2
 if ! [ -s $basedir/$genus.vec.fa ]; then
-	cat $genus/ncbi_dataset/data/*/*.fna > $basedir/$genus.vec.fa
+        cat $genus/ncbi_dataset/data/*/*.fna > $basedir/$genus.vec.fa
 fi
 
 grep ">" $basedir/$genus.vec.fa | sed 's/>//g' | cut -d" " -f1,2,3,4 | sed -r 's/\s+/\t/' > $genus.vectaxmap.tab
@@ -244,21 +244,21 @@ grep ">" $basedir/$genus.vec.fa | sed 's/>//g' | cut -d" " -f1,2,3,4 | sed -r 's
 genus=aedes
 
 if ! [ -s $genus/ncbi_dataset/data ];then
-	echo "Downloadng $genus.... We will let you know when this is done."
-	datasets download genome taxon $genus --dehydrated --filename $genus.zip #There are not complete genomes for aedes
-	unzip $genus.zip -d $genus
-	datasets rehydrate --directory $genus
-	for i in $genus/ncbi_dataset/data/*/*.fna
-	do 
-		kraken2-build --threads 6 --add-to-library $i --db $DBVEC
-	done
-	echo "$genus added to the database"
+        echo "Downloadng $genus.... We will let you know when this is done."
+        datasets download genome taxon $genus --dehydrated --filename $genus.zip #There are not complete genomes for aedes
+        unzip $genus.zip -d $genus
+        datasets rehydrate --directory $genus
+        for i in $genus/ncbi_dataset/data/*/*.fna
+        do
+                kraken2-build --threads 6 --add-to-library $i --db $DBVEC
+        done
+        echo "$genus added to the database"
 else
-	echo "$genus already downloaded. We assume it is already addedd to the $DBVEC"
+        echo "$genus already downloaded. We assume it is already addedd to the $DBVEC"
 fi
 
-if ! [ -s $basedir/$genus.vec.fa]; then
-	cat $genus/ncbi_dataset/data/*/*.fna > $basedir/$genus.vec.fa
+if ! [ -s $basedir/$genus.vec.fa ]; then
+        cat $genus/ncbi_dataset/data/*/*.fna > $basedir/$genus.vec.fa
 fi
 
 grep ">" $basedir/$genus.vec.fa | sed 's/>//g' | cut -d" " -f1,2,3,4 | sed -r 's/\s+/\t/' > $basedir/$genus.vectaxmap.tab
@@ -266,42 +266,44 @@ grep ">" $basedir/$genus.vec.fa | sed 's/>//g' | cut -d" " -f1,2,3,4 | sed -r 's
 #<Culex spp>
 genus=culex
 if ! [ -s $genus/ncbi_dataset/data ];then
-	echo "Downloadng $genus.... We will let you know when this is done."
-	datasets download genome taxon $genus --dehydrated --filename $genus.zip 
-	unzip $genus.zip -d $genus
-	datasets rehydrate --directory $genus
+        echo "Downloadng $genus.... We will let you know when this is done."
+        datasets download genome taxon $genus --dehydrated --filename $genus.zip
+        unzip $genus.zip -d $genus
+        datasets rehydrate --directory $genus
 
-	for i in $genus/ncbi_dataset/data/*/*.fna; do 
-		kraken2-build --threads 6 --add-to-library $i --db $DBVEC; 
-	done
-	echo "$genus added to the database"
+        for i in $genus/ncbi_dataset/data/*/*.fna; do
+                kraken2-build --threads 6 --add-to-library $i --db $DBVEC;
+        done
+        echo "$genus added to the database"
 
 else
-	        echo "$genus already downloaded. We assume it is already addedd to the $DBVEC"
+                echo "$genus already downloaded. We assume it is already addedd to the $DBVEC"
 fi
 
 if ! [ -s $basedir/$genus.vec.fa ]; then
-	cat $genus/ncbi_dataset/data/*/*.fna > $basedir/$genus.vec.fa
+        cat $genus/ncbi_dataset/data/*/*.fna > $basedir/$genus.vec.fa
 fi
 grep ">" $basedir/$genus.vec.fa | sed 's/>//g' | cut -d" " -f1,2,3,4 | sed -r 's/\s+/\t/' > $basedir/$genus.vectaxmap.tab
 
 if ! [ -s $basedir/minimap2_vec.all.fa ]; then
-	cat $basedir/*.vec.fa > $basedir/minimap2_vec.all.fa
+        cat $basedir/*.vec.fa > $basedir/minimap2_vec.all.fa
 fi
 
 MINIVEC=$basedir/minimap2_vec.all.fa
 rm $basedir/*.vec.fa
 cat $basedir/*.vectaxmap.tab > $basedir/vectaxmap.tab
-rm cat $basedir/*.vectaxmap.tab
+rm $basedir/*.vectaxmap.tab
 
 #<Wolbachia spp>
 #genus=wolbachia
-#datasets download genome taxon $genus --dehydrated --filename $genus.zip 
+#datasets download genome taxon $genus --dehydrated --filename $genus.zip
 #unzip $genus.zip -d $genus
 #datasets rehydrate --directory $genus
 #for i in $genus/ncbi_dataset/data/*/*.fna; do kraken2-build --threads 6 --add-to-library $i --db $DBNAME; done
 
-kraken2-build --threads 6 --build --db $DBVEC
+if ! [ -s $DBVEC/taxo.k2d ]; then
+        kraken2-build --threads 6 --build --db $DBVEC
+fi
 
 #Additional references
 #https://cran.r-project.org/web/packages/fs/vignettes/function-comparisons.html
