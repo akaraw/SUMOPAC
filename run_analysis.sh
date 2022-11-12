9c045d9879c85b65fcbde2176ab877cdba1f4927
Anjana_CK <anjana.karawita@csiro.au>
2022-11-12 21:26:04 +1000

    Update run_analysis.sh

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+cdir=$(pwd)
+export PATH=$cdir:$PATH
 echo -e "\
 ###########################################
 # DIRECT RNA SEQ ANALYSIS PIPELINE ACK V1 #
 ###########################################
 "
-if [ "$1" == "-h"]; then
+if [ "$1" == "-h" ]; then
     echo "Usage: run_analysis.sh <sample dir> <work dir> <Basedir> <threads>"
     exit 0
 fi
@@ -15,6 +17,13 @@ fi
 
 # e.g., WDIR=/mnt/c/Users/kar131/OneDrive - CSIRO/CERC_postdoctoral_fellow_Research/SUMOPAC_project
 # e.g., smapledir=/datasets/work/aahl-minion/work/2022/2022-10-24_SUMOPAC_PArty_pool/20221024_0029_MN32206_FAS69300_c19605d4
+sampledir=$1
+WDIR=$2
+basedir=$3 #This is same as step2
+CPUS=$4
+
+export PATH=$basedir/bin:$PATH
+export PATH=$basedir/minimap2:$PATH
 
 if ! command -v minimap2 &> /dev/null
 then
@@ -31,16 +40,14 @@ then
           Please export to the current path"
     exit 1
 else
-    echo "kraken2 found - proceediong to next step"    
+    echo "kraken2 found - proceediong to next step"
 fi
 
-sampledir=$1
-WDIR=$2 
-basedir=$3 #This is same as step2
-CPUS=$4
+mkdir -p $WDIR
+echo "Results will be written to $WDIR"
 
 VECTAX=$basedir/vectaxmap.tab
-BACTAX=$basedir/bactaxmap.tab
+BACTAX=$basedir/bacctaxmap.tab
 VIRTAX=$basedir/virtaxmap.tab
 
 if [ -z $1 ]; then
@@ -63,23 +70,24 @@ fi
 cd $WDIR
 ln -s $sampledir mydir
 
-if [ ! -d "mydir/fastq_pass"]; then
-  echo "Check your sample directory.\
-  There is something wrong with it"
-  exit 1 2> /dev/null
+if ! [ -s "$WDIR/all.fastq" ]; then
+        if ! [ -s "mydir/fastq_pass" ]; then
+                echo "Check your sample directory.\
+                There is something wrong with it"
+                exit 1 2> /dev/null
+        else
+                echo "sample directory contains valid results from\
+                minion run"
+                sdir=mydir/fastq_pass
+                cat $sdir/*.fastq > $WDIR/all.fastq
+                FQ=$WDIR/all.fastq
+        fi
 else
-    echo "sample directory contains valid results from\
-    minion run"
-    sdir=mydir/fastq_pass
+        echo "Omitting fastq concatenation"
+        FQ=$WDIR/all.fastq
 fi
 
 #Concatenate all fastq files
-if [ -s "$WDIR/all.fastq" ]; then
-    echo "Omitting fastq concatenation"
-else
-    cat $sdir/*.fastq > $WDIR/all.fastq
-    FQ=all.fastq
-fi
 
 if [ -s "$FQ" ]; then #if the file is not empty
   echo "Some data present to work with"
@@ -98,45 +106,42 @@ fi
 #minimap2 run - viruses
 PAF=$WDIR/out_vir.paf
 LIB=$basedir/kraken2vir/library/viral/library.fna
-OUTFILE=$WDIR/minimap2_virus.csv 
+OUTFILE=$WDIR/minimap2_virus.csv
 #Now run minimap2
-minimap2 -t $CPUS -k 13 -x map-ont  $LIB $FQ > $PAF #Smaller k-mer for direct RNA seq data from ONT
-
-if [ -s "$PAF" ]; then
-    $basedir/paf_reader.R $PAF $WDIR  $OUTFILE $VIRTAX 11 #This is a Rscript
+if ! [ -s $PAF ];then
+        minimap2 -t $CPUS -x map-ont  $MINIVEC $FQ > $PAF #Change the -k option if needed
 else
-    echo "$PAF is empty.. exiting minimap2 analysis"
-    rm $PAF
+        if ! [ -s $OUTFILE ];then
+                $cdir/paf_reader.R $PAF $WDIR $OUTFILE $VECTAX 11
+        fi
 fi
 
 #minimap2 run - bacteria
-
 PAF=$WDIR/out_bac.paf
 LIB=$basedir/bac.mmi #This is created in step 2
 OUTFILE=$WDIR/minimap2_bacteria.csv
 #Now run minimap2
-minimap2 -t $CPUS -k 13 -x map-ont  $LIB $FQ > $PAF
 
-if [ -s "$PAF" ]; then
-    $basedir/paf_reader.R $PAF $WDIR $OUTFILE $BACTAX 11
+if ! [ -s $PAF ];then
+        minimap2 -t $CPUS -x map-ont  $MINIVEC $FQ > $PAF #Change the -k option if needed
 else
-    echo "$PAF is empty.. exiting minimap2 analysis"
-    rm $PAF
+        if ! [ -s $OUTFILE ];then
+                $cdir/paf_reader.R $PAF $WDIR $OUTFILE $VECTAX 13
+        fi
 fi
 
 #minimap2 run - vector
-
 PAF=$WDIR/out_vec.paf
 MINIVEC=$basedir/minimap2_vec.all.fa
 OUTFILE=$WDIR/minimap2_vec.csv
 #Now run minimap2
-minimap2 -t $CPUS -x map-ont  $MINIVEC $FQ > $PAF #Change the -k option if needed
 
-if [ -s "$PAF" ]; then
-    $basedir/paf_reader.R $PAF $WDIR $OUTFILE $VECTAX 30
+if ! [ -s $PAF ];then
+        minimap2 -t $CPUS -x map-ont  $MINIVEC $FQ > $PAF #Change the -k option if needed
 else
-    echo "$PAF is empty.. exiting minimap2 analysis"
-    rm $PAF
+        if ! [ -s $OUTFILE ]; then
+                $cdir/paf_reader.R $PAF $WDIR $OUTFILE $VECTAX 30
+        fi
 fi
 
 echo "--- Now running Kraken2 ---"
@@ -144,29 +149,26 @@ DBBAC=$basedir/kraken2bac
 DBVIR=$basedir/kraken2vir
 DBVEC=$basedir/kraken2vec
 
+
 if [ -s "$DBBAC" ]; then
-    kraken2 --threads 12 --quick --output $WDIR/kraken2bac.txt --use-names --db $DBBAC all.fastq
+    kraken2 --threads 12 --quick --output $WDIR/kraken2bac.txt --use-names --db $DBBAC $FQ
 else
     echo "Can not locate $DBBAC for kraken2"
     exit 1
 fi
 
 if [ -s "$DBVIR" ]; then
-    kraken2 --threads 12 --quick --output $WDIR/kraken2vir.txt --use-names --db $DBVIR all.fastq
+    kraken2 --threads 12 --quick --output $WDIR/kraken2vir.txt --use-names --db $DBVIR $FQ
 else
     echo -e "$DBVIR does not exist"
     exit 1
 fi
 
 if [ -s "$DBVEC" ]; then
-    kraken2 --threads 12 --quick --output $WDIR/kraken2vec.txt --use-names --db $DBVEC all.fastq
+    kraken2 --threads 12 --quick --output $WDIR/kraken2vec.txt --use-names --db $DBVEC $FQ
 else
     echo -e "Can not locate $DBVEC for kraken2"
     exit 1
 fi
 
 echo -e "Pipeline is now completed - \nCherio!!!"
-
-
-
-
